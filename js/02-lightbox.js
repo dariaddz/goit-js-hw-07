ed9a9357a380203b63d90cbe0f0049ecd2da344f
dariaddz <dariadz@gmail.com>
2021-11-13 14:05:20 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title = "${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a");
+
+lightbox.options.captionDelay = 250;
+
+galleryContainer.addEventListener("click", (evt) => {
+  evt.preventDefault();
+});
